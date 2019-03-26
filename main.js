62f35f3e1135947411d9e9af7c1ae505c10c28c9
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-26 17:47:03 -0400

    made function to add an input to string display. Need to then evaluate the string

--- main.js
+++ main.js
@@ -3,70 +3,111 @@
 let number1 = 0
 let number2 = 0
 let answer = 0
+let display = ''
+let input
+function addValueToDisplay(input) {
+    display += input
+    console.log(display)
+    return display
+}
+
 
 document.querySelector('.button0').addEventListener('click', function (event) {
     console.log("pushed 0 button", 0)
     document.querySelector('.display').innerText += 0
-    document.querySelector('button0').addEventListener('click', )
+    input = 0
+    addValueToDisplay(input)
 })
 document.querySelector('.button1').addEventListener('click', function (event) {
     console.log(1)
     document.querySelector('.display').innerText += 1
+    input = 1
+    addValueToDisplay(input)
 })
 document.querySelector('.button2').addEventListener('click', function (event) {
     console.log(2)
     document.querySelector('.display').innerText += 2
+    input = 2
+    addValueToDisplay(input)
 })
 document.querySelector('.button3').addEventListener('click', function (event) {
     console.log(3)
     document.querySelector('.display').innerText += 3
+    input = 3
+    addValueToDisplay(input)
 })
 document.querySelector('.button4').addEventListener('click',function (event) {
     console.log(4)
     document.querySelector('.display').innerText += 4
+    input = 4
+    addValueToDisplay(input)
 })
 document.querySelector('.button5').addEventListener('click', function (event) {
     console.log(5)
     document.querySelector('.display').innerText += 5
+    input = 5
+    addValueToDisplay(input)
 })
 document.querySelector('.button6').addEventListener('click', function (event) {
     console.log(6)
     document.querySelector('.display').innerText += 6
+    input = 6
+    addValueToDisplay(input)
 })
 document.querySelector('.button7').addEventListener('click', function (event) {
     console.log(7)
     document.querySelector('.display').innerText += 7
+    input = 7
+    addValueToDisplay(input)
 })
 document.querySelector('.button8').addEventListener('click', function (event) {
     console.log(8)
     document.querySelector('.display').innerText += 8
+    input = 8
+    addValueToDisplay(input)
 })
 document.querySelector('.button9').addEventListener('click', function (event) {
     console.log(9)
     document.querySelector('.display').innerText += 9
+    input = 9
+    addValueToDisplay(input)
 })
 document.querySelector('.add').addEventListener('click', function (event) {
     console.log('+')
     document.querySelector('.display').innerText += '+'
+    input = '+'
+    addValueToDisplay(input)
 })
 document.querySelector('.sub').addEventListener('click', function (event) {
     console.log(' - ')
     document.querySelector('.display').innerText += '-'
+    input = '-'
+    addValueToDisplay(input)
 })
 document.querySelector('.multiply').addEventListener('click', function (event) {
     console.log(' x ')
-    document.querySelector('.display').innerText += '-'
+    document.querySelector('.display').innerText += 'x'
+    input = '*'
+    addValueToDisplay(input)
 })
 document.querySelector('.divide').addEventListener('click', function (event) {
     console.log(' / ')
     document.querySelector('.display').innerText += '/'
+    input = '/'
+    addValueToDisplay(input)
 })
 document.querySelector('.decimal').addEventListener('click', function (event) {
     console.log('.')
     document.querySelector('.display').innerText += '.'
+    input = '.'
+    addValueToDisplay(input)
 })
 document.querySelector('.buttonClear').addEventListener('click', function (event) {
     console.log('clear')
     document.querySelector('.display').innerText = ''
 })
-
+document.querySelector('.equal').addEventListener('click', function (event) {
+    console.log('=')
+    document.querySelector('.display').innerText = '='
+    //then evaluate the display
+})
