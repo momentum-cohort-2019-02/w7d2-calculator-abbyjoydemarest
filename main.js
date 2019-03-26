522552605cb5f8536f2a027e66b6c0c88fd9ea57
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-26 15:06:04 -0400

    show value of button in input when button clicked

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
-function add(number1, number2)
+let number1 = 0
+let number2 = 0
+let answer = 0
+
+document.querySelector('.button0').onclick = function (event) {
+    console.log(0)
+    document.querySelector('.input').innerText += 0
+}
+document.querySelector('.button1').onclick = function (event) {
+    console.log(1)
+    document.querySelector('.input').innerText += 1
+}
+document.querySelector('.button2').onclick = function (event) {
+    console.log(2)
+    document.querySelector('.input').innerText += 2
+}
+document.querySelector('.button3').onclick = function (event) {
+    console.log(3)
+    document.querySelector('.input').innerText += 3
+}
+document.querySelector('.button4').onclick = function (event) {
+    console.log(4)
+    document.querySelector('.input').innerText += 4
+}
+document.querySelector('.button5').onclick = function (event) {
+    console.log(5)
+    document.querySelector('.input').innerText += 5
+}
+document.querySelector('.button6').onclick = function (event) {
+    console.log(6)
+    document.querySelector('.input').innerText += 6
+}
+document.querySelector('.button7').onclick = function (event) {
+    console.log(7)
+    document.querySelector('.input').innerText += 7
+}
+document.querySelector('.button8').onclick = function (event) {
+    console.log(8)
+    document.querySelector('.input').innerText += 8
+}
+document.querySelector('.button9').onclick = function (event) {
+    console.log(9)
+    document.querySelector('.input').innerText += 9
+}
+document.querySelector('.add').onclick = function (event) {
+    console.log('+')
+    document.querySelector('.input').innerText += '+'
+}
+document.querySelector('.sub').onclick = function (event) {
+    console.log(' - ')
+    document.querySelector('.input').innerText += '-'
+}
+document.querySelector('.multiply').onclick = function (event) {
+    console.log(' x ')
+    document.querySelector('.input').innerText += '-'
+}
+document.querySelector('.divide').onclick = function (event) {
+    console.log(' / ')
+    document.querySelector('.input').innerText += '/'
+}
+document.querySelector('.decimal').onclick = function (event) {
+    console.log('.')
+    document.querySelector('.input').innerText += '.'
+}
+
